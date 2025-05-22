d550f89d56cf6ecd07b896d22024ef8b04fde566
FernandoSalicandro <studio.salicandro@gmail.com>
2025-05-22 17:22:47 +0200

    feat: bonus completato

--- blog.js
+++ blog.js
@@ -2,6 +2,11 @@ console.log("////////////////////////");
 
 //importo e assegno alla costante express il modulo express
 import express from "express";
+import fs from "fs";
+
+const data = fs.readFileSync("blog-post.json", "utf-8" );
+const blogPost = JSON.parse(data)
+
 //creo l'app server
 const app = express();
 //assegno la porta da cui il server si metterà in ascolto.
@@ -10,41 +15,6 @@ const port = 3000;
 
 //servo i file statici presenti in cartella images. Ora sono accessibili da url.
 app.use(express.static("images"));
-
-// Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
-
-const blogPost = [
-    { 
-        titolo: "Il classico ciambellone della nonna",
-        contenuto: "Una ricetta semplice e genuina per un dolce soffice perfetto a colazione o merenda.",
-        immagine: "http://localhost:3000/ciambellone.jpeg",
-        tags: ["dolci", "colazione", "tradizione", "ricetta"]
-    },
-    {
-        titolo: "Cracker alla barbabietola con crema di formaggio",
-        contenuto: "Colorati, croccanti e sorprendenti: scopri come preparare questi cracker sfiziosi e salutari.",
-        immagine: "http://localhost:3000/cracker_barbabietola.jpeg",
-        tags: ["antipasti", "salutare", "vegetariano", "ricetta"]
-    },
-    {
-        titolo: "Pane fritto dolce: un tuffo nei ricordi",
-        contenuto: "Una ricetta povera ma ricca di gusto, perfetta per recuperare il pane avanzato con un tocco di zucchero e cannella.",
-        immagine: "http://localhost:3000/pane_fritto_dolce.jpeg",
-        tags: ["dolci", "tradizione", "riciclo", "merenda"]
-    },
-    {
-        titolo: "Pasta alla barbabietola e pistacchi",
-        contenuto: "Un primo piatto dai colori vivaci e dai sapori decisi, perfetto per stupire i tuoi ospiti.",
-        immagine: "http://localhost:3000/pasta_barbabietola.jpeg",
-        tags: ["primi", "vegetariano", "barbabietola", "creativo"]
-    },
-    {
-        titolo: "Torta paesana: il dolce della tradizione lombarda",
-        contenuto: "Pane raffermo, cacao, pinoli e uvetta: ingredienti semplici per un dolce ricco di storia.",
-        immagine: "http://localhost:3000/torta_paesana.jpeg",
-        tags: ["dolci", "lombardia", "tradizione", "ricetta"]
-    }
-];
 
 
 //definisco una rotta per le richieste GET all'url di root "/"
